b26573575b650cb9eb33714e6146cf3b5a373b16
aniket59400 <aniketw100@gmail.com>
2026-05-29 17:23:15 +0530

    Fixed TestNG parallel execution issue

--- src/test/java/tests/ui/LoginTest.java
+++ src/test/java/tests/ui/LoginTest.java
@@ -1,10 +1,12 @@
 package tests.ui;
 
 import base.BaseTest;
+import drivers.DriverFactory;
 import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import pages.LoginPage;
+import pages.NotesPage;
 
 @Listeners(listeners.TestListener.class)
 public class LoginTest extends BaseTest {
@@ -12,8 +14,8 @@ public class LoginTest extends BaseTest {
     @Test(description = "TC-UI-01")
     public void validLoginTest() {
 
-        LoginPage lp =
-                new LoginPage();
+        LoginPage lp = new LoginPage();
+        NotesPage np = new NotesPage();
 
         lp.openApp();
 
@@ -22,9 +24,30 @@ public class LoginTest extends BaseTest {
                 "pass123"
         );
 
-        // basic assertion: add note button should be visible after successful login
-        pages.NotesPage np = new pages.NotesPage();
-        org.testng.Assert.assertTrue(np.isAddNoteVisible(), "Add note button should be visible after login");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("================================");
+        System.out.println("CURRENT URL = "
+                + DriverFactory.getDriver().getCurrentUrl());
+
+        System.out.println("PAGE TITLE = "
+                + DriverFactory.getDriver().getTitle());
+
+        System.out.println("PAGE CONTAINS 'Add Note' = "
+                + DriverFactory.getDriver()
+                .getPageSource()
+                .contains("Add Note"));
+
+        System.out.println("================================");
+
+        Assert.assertTrue(
+                np.isAddNoteVisible(),
+                "Add note button should be visible after login"
+        );
     }
 
     @Test(description = "TC-NEG-01")
